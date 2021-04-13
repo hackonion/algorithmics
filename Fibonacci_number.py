339efb42b05892acb1e2f48cb123300341104c79
hackonion <jorge21913@hotmail.com>
2021-04-13 12:59:35 -0500

    Fibonacci complete update

--- Fibonacci_number.py
+++ Fibonacci_number.py
@@ -4,17 +4,14 @@
     called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
 """
 
-#Recursion solution
-from typing import Counter
-
-
+#Using a recursive solution
 def fib(n: int) -> int:
     if n <= 1:
         return n
     
     return fib(n-1) + fib(n-2)
 
-#Usin a Iterative  
+#Using a iterative solution
 def fib_for(n:int) -> int:
     
     prev = 1
